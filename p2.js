3488589d3055a8cc24cc62dd8242502279518df9
Kritii <kritikumari53782@gmail.com>
2026-03-25 10:35:12 +0530

    added

---
+++ p2.js
@@ -0,0 +1,13 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+const colors = [
+    "#cdb4db",
+    "#bde0fe",
+    "#eecfed",
+    "#d1c4e9",
+    "#9edee7",
+    "#ffe5ec",
+    "#b8c0ff"
+];
+
